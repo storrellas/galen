367da6a76a82de24026151bf1b8f6593a4ec764a
storrellas <storrellas@gmail.com>
2020-02-07 13:32:45 +0100

    ENH: Adding feature path

--- src/test/java/com/galen/cucumber/testng/RunCucumberTest.java
+++ src/test/java/com/galen/cucumber/testng/RunCucumberTest.java
@@ -3,6 +3,6 @@ package cucumbertest.testng;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(plugin = "json:target/cucumber-report.json")
+@CucumberOptions(plugin = "json:target/cucumber-report.json", features="src/test/resources")
 public class RunCucumberTest extends AbstractTestNGCucumberTests {
 }
